79fcc039efa7b3ecafdd3473441f73cb7864591e
apdelrahman <apdelrahman@192.168.245.128>
2019-07-07 18:11:26 -0700

    ios update to be like android

--- Src/Root.js
+++ Src/Root.js
@@ -1,5 +1,6 @@
 const Root = {
     Proudction: 'https://yfitness.herokuapp.com',
     Test:       'https://yfitnesstest.herokuapp.com'
+    //Test:       'https://y-fitness.herokuapp.com'
 }
 export const MainUrl = Root.Test
--- Src/Screens/LoadingScreen.js
+++ Src/Screens/LoadingScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, View, ActivityIndicator } from 'react-native';
+import { Image, View, ActivityIndicator , AsyncStorage } from 'react-native';
 import backround from '../Photo/Logo.png';
 import { getToken, HowIsMe } from '../Screens/Application';
 import Button from '../Component/Button';
@@ -41,13 +41,19 @@ class Loading extends React.Component {
                 <Image source={backround} resizeMode='stretch' style={{ alignSelf: 'center', height: 200, width: 200 }} />
                 {this.state.Loading == true ?
                     <ActivityIndicator size='large' color='#fcb72b' /> :
+                    <View>
                     <Button 
                     Text='ReTry' 
-                    textStyle={{ alignSelf: 'center', color: 'black',          fontSize: 20 }}
+                    textStyle={{ alignSelf: 'center', color: 'black', fontFamily: Fonts.Helvetica, fontSize: 20 }}
                         style={{ backgroundColor: '#fcb72b', padding: 10, marginTop: 40 }}
                     onPress={() => {
                         this.Init()
                     }} />
+                    <Button 
+                    style={{ backgroundColor: '#1a1a1a', padding: 10, marginTop: 330 , width: 60  }}
+                    textStyle={{ alignSelf: 'center', color: 'black', fontFamily: Fonts.Helvetica, fontSize: 10 }} Text='Log out' onPress={() => { AsyncStorage.clear().then(() => { this.props.navigation.navigate('Auth') }) }}
+                    />
+                </View> 
                 }
             </View>
         )
